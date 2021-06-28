585213880e8ef9ef10b2b12c3b92ca6be52b7925
Isaac Lim <limshengyangisaac@gmail.com>
2021-06-28 14:03:29 +0800

    5th Commit (Using Date)

--- App.js
+++ App.js
@@ -61,3 +61,21 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+  // Remember the latest callback.
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  // Set up the interval.
+  useEffect(() => {
+    function tick() {
+      savedCallback.current();
+    }
+    if (delay !== null) {
+      let id = setInterval(tick, delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import {
   Text,
@@ -7,69 +7,131 @@ import {
   StyleSheet,
   Dimensions,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 import { List, Card, Paragraph, Title, ProgressBar } from "react-native-paper";
 import { MaterialIcons, AntDesign } from "@expo/vector-icons";
 
-var completionTime = "1h20m";
-var chargePercentage = 0.75;
+import useInterval from "../App.js";
+import { queue } from "async";
+
+var completionTime = "?";
+var queueTime = "?";
+
+var chargePercentage = 90;
+var queueNumber = 0;
+var timePerCar = 30;
+
+var timeTracker = timePerCar;
 
 var parkLocation = "VivoCity Multi-Storey Carpark";
 var parkLevel = "Level 4";
 var parkZone = "Zone C";
 var parkLot = "Lot 35";
 
-var queueNumber = 1;
-var startTime = "1:24PM";
-var endTime = "3:17PM";
-
 function HomeScreen({ navigation }) {
   let windowWidth = Dimensions.get("window").width;
   let chargingMessage, timeMessage;
 
   const [carCharging, setCarCharging] = useState(false);
-  const [carParked, setCarParked] = useState(true);
+  const [carParked, setCarParked] = useState(false);
   const [carQueueing, setCarQueueing] = useState(false);
+
+  const [startTime, setStartTime] = useState(new Date().toLocaleString());
+  const [endTime, setEndTime] = useState(new Date().toLocaleString());
+  const [pressTime, setPressTime] = useState(new Date().toLocaleString());
+  const [currentTime, setCurrentTime] = useState(new Date().toLocaleString());
 
   if (carCharging) {
     chargingMessage = "Press Car to Cancel Charging";
-    timeMessage = completionTime + " to full charge";
+    if (completionTime == "?") {
+      timeMessage = "Calculating time to full charge...";
+    } else {
+      timeMessage = completionTime + "s to full charge";
+    }
   } else if (carParked && carQueueing) {
     chargingMessage = "Press Car to Cancel Queueing";
-    timeMessage = completionTime + " to full charge";
+    if (queueTime == "?") {
+      timeMessage = "Calculating time till your turn...";
+    } else {
+      timeMessage = "Estimated " + queueTime + "s till your turn";
+    }
   } else {
     chargingMessage = "Press Car to Start Charging";
     timeMessage = "";
   }
   const changeChargeState = () => {
+    setPressTime(currentTime);
+
     if (carCharging) {
       setCarCharging(false);
       setCarQueueing(false);
     } else if (carParked && !carCharging && queueNumber == 0) {
       setCarCharging(true);
       setCarQueueing(true);
+
+      setStartTime(currentTime);
+      setEndTime(currentTime + completionTime);
     } else if (carParked && !carCharging && queueNumber != 0 && !carQueueing) {
       setCarCharging(false);
       setCarQueueing(true);
+
+      queueTime = queueNumber * timePerCar;
     } else if (carParked && !carCharging && queueNumber != 0 && carQueueing) {
       setCarCharging(false);
       setCarQueueing(false);
+    } else if (!carParked) {
+      Alert.alert("Car not Parked", "Unable to start charging");
     }
 
     if (carCharging) {
       timeMessage = completionTime + " to full charge";
     } else if (carParked && carQueueing) {
-      timeMessage = completionTime + " to full charge";
+      timeMessage = queueTime + " to charge start";
     } else {
       timeMessage = "";
     }
   };
+
+  useEffect(() => {
+    let secTimer = setInterval(() => {
+      setCurrentTime(new Date().toLocaleString());
+    }, 1000);
+
+    return () => clearInterval(secTimer);
+  }, []);
+
+  useEffect(() => {
+    if (carCharging && chargePercentage < 100) {
+      chargePercentage += 1;
+      completionTime = 100 - chargePercentage;
+    } else if (carCharging && chargePercentage >= 100) {
+      setCarCharging(false);
+      setCarQueueing(false);
+      Alert.alert("Charging Complete", "Charging Process Stopped");
+    } else if (carQueueing && queueNumber > 0) {
+      if (timeTracker == 0) {
+        timeTracker = timePerCar;
+        queueNumber--;
+      } else {
+        timeTracker--;
+      }
+
+      queueTime = (queueNumber - 1) * timePerCar + timeTracker;
+
+      if (queueNumber == 0) {
+        setCarCharging(true);
+        setCarQueueing(true);
+      }
+    }
+  }, [currentTime]);
 
   return (
     <SafeAreaView style={styles.container}>
       <ScrollView style={styles.minicontainer}>
         <Card style={styles.card}>
           <Card.Content>
+            <Text style={styles.chargemessage}>{currentTime}</Text>
             <Text style={styles.chargemessage}>{chargingMessage}</Text>
             <TouchableOpacity onPress={changeChargeState}>
               <MaterialIcons
@@ -119,20 +181,24 @@ function HomeScreen({ navigation }) {
             </SafeAreaView>
             <ProgressBar
               style={{ marginTop: 5 }}
-              progress={chargePercentage}
+              progress={chargePercentage / 100}
               color="#4169E1"
             ></ProgressBar>
             <Text style={styles.subtitle}>
-              {chargePercentage * 100 + "% Charged"}
+              {chargePercentage + "% Charged"}
             </Text>
             <Text style={styles.title}>{timeMessage}</Text>
           </Card.Content>
         </Card>
         <Card style={styles.card}>
           <Card.Content>
-            <Title style={styles.title}>{parkLocation}</Title>
-            <Text style={styles.subtitle}>{parkLevel + " " + parkZone}</Text>
-            <Text style={styles.subtitle}>{parkLot}</Text>
+            <Title style={styles.title}>
+              {carParked ? parkLocation : "Car Not Parked"}
+            </Title>
+            <Text style={styles.subtitle}>
+              {carParked ? parkLevel + " " + parkZone : ""}
+            </Text>
+            <Text style={styles.subtitle}>{carParked ? parkLot : ""}</Text>
           </Card.Content>
           <Card.Content>
             <SafeAreaView style={styles.tablecolumn}>
